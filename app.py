54431d12c0f37b7e91d851d71031d0415812be10
ditohafizh <ditohafizh@gmail.com>
2019-04-29 10:01:45 +0700

    add db init

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-print(os.environ['APP_SETTINGS'])
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Result
 
 @app.route('/')
 def hello():
